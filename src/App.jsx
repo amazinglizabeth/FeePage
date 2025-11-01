ce167181763a1c4770c35fcea766f93d09ee7852
amazing lizabeth <eadenike14@gmail.com>
2025-11-01 00:53:06 +0000

    fix case sensitive name issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from "@/components/Header.jsx";
 import Hero from "@/components/Hero.jsx";
 import FeeStructure from "@/components/Fee-Structure.jsx";
 import ExchangeCalculator from "@/components/Exchange-Calculator.jsx";
-import VirtualCard from "@/components/VirtualCard.jsx";
 import WhereYourFeesGo from "@/components/Where-your-fees-go.jsx";
 import FAQ from "@/components/FAQ.jsx";
 import Referrals from "@/components/Referrals.jsx";
@@ -11,6 +10,7 @@ import TrustLogo from "@/components/TrustLogo.jsx";
 import Cta from "@/components/Cta.jsx";
 import Footer from "@/components/Footer.jsx";
 import ChatterBox from "@/components/ChatterBox.jsx";
+import VirtualCard from "./components/VirtualCard";
 
 export default function App() {
   return (
