68996ae92875a98915b2bc0bb0be03b13df0bdc0
amazing lizabeth <eadenike14@gmail.com>
2025-11-01 00:59:12 +0000

    fix case sensitive name issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import TrustLogo from "@/components/TrustLogo.jsx";
 import Cta from "@/components/Cta.jsx";
 import Footer from "@/components/Footer.jsx";
 import ChatterBox from "@/components/ChatterBox.jsx";
-import VirtualCard from "./components/VirtualCard";
+import VirtualCard from "./components/VirtualCard.jsx";
 
 export default function App() {
   return (
